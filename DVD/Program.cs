54ba6caa732d307f66e8d999255d8437f49b9de3
Siumeli <sameli.aro@edu.bc.fi>
2025-01-17 12:38:01 +0200

    Raylib lisätty

--- DVD/Program.cs
+++ DVD/Program.cs
@@ -6,5 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        //Raylib Lisätty
     }
 }
--- DVD/obj/Debug/net8.0/DVD.AssemblyInfo.cs
+++ DVD/obj/Debug/net8.0/DVD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DVD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d97f2327f6e17182a2086bd944c6659d21d016")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c3994ddc534d5675c37929b2a6f594fc9d1a610")]
 [assembly: System.Reflection.AssemblyProductAttribute("DVD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DVD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
